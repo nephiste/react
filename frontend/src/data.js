a379cf52f423ee587d424ca5480f900940d1b851
nephiste <119631037+nephiste@users.noreply.github.com>
2023-01-07 12:28:17 +0100

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+const data = {
+    products: [
+        {
+            name: 'Green tea',
+            slug: 'green-tea',
+            category: 'Teas',
+            image: '/images/p1.jpg',
+            price: 20,
+            countInStock: 10;
+            brand: 'Lipton',
+            rating: 3,
+            numReviews: 8,
+            description: 'high quality tea',
+        },
+        {
+            name: 'Black tea',
+            slug: 'Black-tea',
+            category: 'Teas',
+            image: '/images/p2.jpg',
+            price: 50,
+            countInStock: 20;
+            brand: 'Nelson',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality black tea',
+        },
+        {
+            name: 'Yerba mate',
+            slug: 'yerba-mate',
+            category: 'Yerba',
+            image: '/images/p3.jpg',
+            price: 100,
+            countInStock: 5;
+            brand: 'Mate',
+            rating: 5,
+            numReviews: 16,
+            description: 'Energizing yerba mate',
+        },
+    ]
+}
