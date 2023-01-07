936fa864160ee564f7094dcc31cad2d126611b10
nephiste <119631037+nephiste@users.noreply.github.com>
2023-01-07 13:21:59 +0100

    third-commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,13 +13,15 @@ import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SignInScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { cart, userInfo } = state;
 
   const signoutHandler = () => {
     ctxDispatch({ type: 'USER_SIGNOUT' });
-    localStorage.Storage.removeItem('userInfo');
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('shippingAddress');
   };
 
   return (
@@ -81,6 +83,10 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route
+                path="/shipping"
+                element={<ShippingAddressScreen />}
+              ></Route>
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
