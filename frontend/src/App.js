3af10017cf5be0d6eeef5128bfb8cf1b19098044
nephiste <119631037+nephiste@users.noreply.github.com>
2023-01-08 15:01:14 +0100

    PaypalTransaction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ function App() {
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>Herbaciarnia.pl</Navbar.Brand>
               </LinkContainer>
               <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
