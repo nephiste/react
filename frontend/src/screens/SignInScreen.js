661bbde1ccf54f3413adf14064db4e1c0b8a3d12
nephiste <119631037+nephiste@users.noreply.github.com>
2023-01-10 17:33:09 +0100

    SuicidalThoughts

--- frontend/src/screens/SignInScreen.js
+++ frontend/src/screens/SignInScreen.js
@@ -4,74 +4,78 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
-import { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Store } from '../Store';
 import { toast } from 'react-toastify';
 import { getError } from '../utils';
 
-export default function SigninScreen() {
-  const navigate = useNavigate();
-  const { search } = useLocation();
-  const redirectInUrl = new URLSearchParams(search).get('redirect');
-  const redirect = redirectInUrl ? redirectInUrl : '/';
+export default class SigninScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    this.submitHandler = this.submitHandler.bind(this);
+  }
 
-  const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { userInfo } = state;
-  const submitHandler = async (e) => {
+  static contextType = Store;
+
+  submitHandler = async (e) => {
     e.preventDefault();
     try {
       const { data } = await Axios.post('/api/users/signin', {
-        email,
-        password,
+        email: this.state.email,
+        password: this.state.password,
       });
-      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      this.context.dispatch({ type: 'USER_SIGNIN', payload: data });
       localStorage.setItem('userInfo', JSON.stringify(data));
-      navigate(redirect || '/');
+      window.location.href = '/';
     } catch (err) {
       toast.error(getError(err));
     }
   };
 
-  useEffect(() => {
-    if (userInfo) {
-      navigate(redirect);
+  componentDidMount() {
+    if (this.context.state.userInfo) {
+      window.location.href = '/';
     }
-  }, [navigate, redirect, userInfo]);
-
-  return (
-    <Container className="small-container">
-      <Helmet>
-        <title>Sign In</title>
-      </Helmet>
-      <h1 className="my-3">Sign In</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <div className="mb-3">
-          <Button type="submit">Sign In</Button>
-        </div>
-        <div className="mb-3">
-          New customer?{' '}
-          <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
-        </div>
-      </Form>
-    </Container>
-  );
+  }
+  render() {
+    return (
+      <Container className="small-container">
+        <Helmet>
+          <title>Sign In</title>
+        </Helmet>
+        <h1 className="my-3">Sign In</h1>
+        <Form onSubmit={this.submitHandler}>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              required
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              required
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+          </Form.Group>
+          <div className="mb-3">
+            <Button type="submit">Sign In</Button>
+          </div>
+          <div className="mb-3">
+            New customer? <Link to={`/signup`}>Create your account</Link>
+          </div>
+        </Form>
+      </Container>
+    );
+  }
 }
